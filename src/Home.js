c06eb80fc3881dd01329b269249e00965693da4a
matvb <mateus.villasboas@gmail.com>
2021-03-05 11:23:00 -0300

    implementing material-ui

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import Bio from './components/Bio';
+import Education from './components/Education';
+import Button from '@material-ui/core/Button';
+import Slide from '@material-ui/core/Slide';
 
 function Home(props) {
     const [isBackgroundClass, setBackgroundClass] = useState('main-background');
@@ -27,32 +30,45 @@ function Home(props) {
 
 
     return (
-    <header className={'App-header ' + isBackgroundClass}>
-        <div className="main-menu">
-            <h1 className="title-name" onClick={() => clickTitle() }>Mateus Villas Boas</h1>
-            <div className='row'>        
-                <div className='col'>
-                    <div className='btn' onClick={() => clickBio()}>
-                        Bio
+    <div>
+        <header className={'App-header ' + isBackgroundClass}>
+            <div className="main-menu">
+                <h1 className="title-name" onClick={() => clickTitle() }>Mateus Villas Boas</h1>
+                <div className='row'>        
+                    <div className='col'>
+                        <div className='btn' onClick={() => clickBio()}>
+                            Bio
+                        </div>
                     </div>
-                </div>
-                <div className='col'>
-                    <div className='btn' onClick={() => clickEducation()}>
-                        Education
+                    <div className='col'>
+                        <div className='btn' onClick={() => clickEducation()}>
+                            Education
+                        </div>
                     </div>
-                </div>
-                <div className='col'>
-                    <div className='btn'>
-                        Experience
+                    <div className='col'>
+                        <Button variant="contained" color="primary" onClick={() => clickEducation()}>
+                            Experience
+                        </Button>
                     </div>
                 </div>
             </div>
-        </div>
+        </header>
         <div className='sections'>
-            <Bio classes={isBioActive ? 'active' : 'deactive'} />
+            <div className='row justify-content-end'>
+                <Slide direction="right" timeout={1000} in={isEducationActive}   >
+                    <h1 style={{color: 'black'}}> UFRJ </h1>
+                </Slide>
+                <Slide direction="left" timeout={1000} in={isBioActive}   >
+                    <div className='col-6'>
+                        <Bio classes={'active'} />
+
+                    </div>
+                </Slide>
+            </div>
+        
 
         </div>
-    </header>
+     </div>
     );
 }
 
---
+++ src/components/Education.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Education(props) {
+
+
+    return (
+    <section className={props.classes + ' bio-section'} style={{color: 'black'}}>
+        <h2> Education </h2>
+        <p>UFRJ</p>
+    </section>
+    );
+}
+
+export default Education;
